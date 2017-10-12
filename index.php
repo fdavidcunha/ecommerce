cceeb5d6a1b8009291d2609b01c8d59b827d31a1
david ferreira <f.david.cunha@gmail.com>
2017-10-12 12:56:36 -0300

    implementação da página de login

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <?php 
+	session_start();
 
 	require_once("vendor/autoload.php");
 
 	use \Slim\Slim;
 	use \Hcode\Page;
 	use \Hcode\PageAdmin;
+	use \Hcode\Model\User;
 
 	$app = new Slim();
 
 	$app->config('debug', true);
 
+	/* Rota para a index do site */
 	$app->get('/', function() {
     
 		$page = new Page();
@@ -17,10 +20,43 @@
 
 	});
 
+	/* Rota para a index do admin */
 	$app->get('/admin', function() {
     
+		User::verifyLogin();
+
 		$page = new PageAdmin();
 		$page->setTpl("index");
+
+	});
+
+	/* Rota para a página de login */
+	$app->get('/admin/login', function() {
+    
+		$page = new PageAdmin( [ 
+			"header" => false, 
+			"footer" => false 
+		] );
+
+		$page->setTpl("login");
+
+	});
+
+	/* Rota para a página de login */
+	$app->post('/admin/login', function() {
+    
+		User::login( $_POST[ 'deslogin' ], $_POST[ 'despassword' ] );
+		header( "Location:/admin" );
+		exit;
+
+	});
+
+	$app->get('/admin/logout', function() {
+
+		User::logout();
+
+		header("Location:/admin/login");
+		exit;
 
 	});
 
