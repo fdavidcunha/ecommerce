d32e1c4659d1ee44cf27d596384a02b9907bf833
david ferreira <f.david.cunha@gmail.com>
2017-11-07 14:05:07 -0300

    Resumo do carrinho

--- site.php
+++ site.php
@@ -234,6 +234,7 @@
 		$address->save();
 
 		$cart = Cart::getFromSession();
+		$cart->getCalculateTotal();
 
 		$order = new Order();
 		$order->setData( [
@@ -241,7 +242,7 @@
 			'idaddress' => $address->getidaddress(),
 			'iduser'    => $user->getiduser(),
 			'idstatus'  => OrderStatus::EM_ABERTO,
-			'vltotal'   => $cart->getCalculateTotal() + $cart->getvlfreight()
+			'vltotal'   =>  + $cart->getvltotal()
 		] );
 
 		$order->save();
@@ -495,65 +496,140 @@
 		$order->get( (int)$idorder );
 
 		// DADOS DO BOLETO PARA O SEU CLIENTE
+		
+		// Dias para pagamento.
 		$dias_de_prazo_para_pagamento = 3;
+		
+		// Taxa de emissão de boleto.
 		$taxa_boleto = 5.00;
-		$data_venc = date("d/m/Y", time() + ($dias_de_prazo_para_pagamento * 86400));  // Prazo de X dias OU informe data: "13/04/2006"; 
-		$valor_cobrado = formatPrice( $order->getvlrotal() ); // Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal
-		$valor_cobrado = str_replace(",", ".",$valor_cobrado);
-		$valor_boleto=number_format($valor_cobrado+$taxa_boleto, 2, ',', '');
 
-		$dadosboleto["nosso_numero"] = $order->getidorder();  // Nosso numero - REGRA: Máximo de 8 caracteres!
-		$dadosboleto["numero_documento"] = $order->getidorder();	// Num do pedido ou nosso numero
-		$dadosboleto["data_vencimento"] = $data_venc; // Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA
-		$dadosboleto["data_documento"] = date("d/m/Y"); // Data de emissão do Boleto
-		$dadosboleto["data_processamento"] = date("d/m/Y"); // Data de processamento do boleto (opcional)
-		$dadosboleto["valor_boleto"] = $valor_boleto; 	// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula
+		// Data de vencimento do título.
+		$data_venc = date( "d/m/Y", time() + ( $dias_de_prazo_para_pagamento * 86400 ) );
+		
+		// Valor - REGRA: Sem pontos na milhar e tanto faz com "." ou "," ou com 1 ou 2 ou sem casa decimal.
+		$valor_cobrado = formatPrice( $order->getvltotal() ); 
+		$valor_cobrado = str_replace( ".", "", $valor_cobrado );
+		$valor_cobrado = str_replace( ",", ".", $valor_cobrado );
+		$valor_boleto  = number_format( $valor_cobrado + $taxa_boleto, 2, ',', '' );
+
+		// Nosso numero - REGRA: Máximo de 8 caracteres.
+		$dadosboleto[ "nosso_numero" ] = $order->getidorder();  
+
+		// Numero do pedido ou nosso numero.
+		$dadosboleto[ "numero_documento" ] = $order->getidorder();	
+
+		// Data de Vencimento do Boleto - REGRA: Formato DD/MM/AAAA.
+		$dadosboleto[ "data_vencimento" ] = $data_venc; 
+
+		// Data de emissão do Boleto.
+		$dadosboleto[ "data_documento" ] = date( "d/m/Y" ); 
+
+		// Data de processamento do boleto (opcional).
+		$dadosboleto[ "data_processamento" ] = date("d/m/Y"); 
+
+		// Valor do Boleto - REGRA: Com vírgula e sempre com duas casas depois da virgula.
+		$dadosboleto[ "valor_boleto" ] = $valor_boleto; 	
 
 		// DADOS DO SEU CLIENTE
-		$dadosboleto["sacado"] = $order->getdesperson();
-		$dadosboleto["endereco1"] = $order->getdesaddress() . ", " . $order->getdesdistrict();
-		$dadosboleto["endereco2"] =  $order->getdescity() . " - " . $order->getdesstate() . " - " . $order->getdescountry() . " - CEP: " . $order->getdeszipcode();
+		
+		// Nome do cliente.
+		$dadosboleto[ "sacado"] = $order->getdesperson();
+		
+		// Primeira parte do endereço.
+		$dadosboleto[ "endereco1" ] = $order->getdesaddress() . ", " . $order->getdesdistrict();
+		
+		// Segunda parte do endereço.
+		$dadosboleto[ "endereco2" ] =  $order->getdescity() . " - " . 
+		                               $order->getdesstate() . " - " . 
+		                               $order->getdescountry() . " - CEP: " . 
+		                               $order->getdeszipcode();
 
 		// INFORMACOES PARA O CLIENTE
-		$dadosboleto["demonstrativo1"] = "Pagamento de Compra na Loja Hcode E-commerce";
-		$dadosboleto["demonstrativo2"] = "Taxa bancária - R$ 0,00";
-		$dadosboleto["demonstrativo3"] = "";
-		$dadosboleto["instrucoes1"] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
-		$dadosboleto["instrucoes2"] = "- Receber até 10 dias após o vencimento";
-		$dadosboleto["instrucoes3"] = "- Em caso de dúvidas entre em contato conosco: suporte@hcode.com.br";
-		$dadosboleto["instrucoes4"] = "&nbsp; Emitido pelo sistema Projeto Loja Hcode E-commerce - www.hcode.com.br";
+		
+		// Mensagens personalizadas.
+		$dadosboleto[ "demonstrativo1" ] = "Pagamento de Compra na Loja Hcode E-commerce";
+		$dadosboleto[ "demonstrativo2" ] = "Taxa bancária - R$ 0,00";
+		$dadosboleto[ "demonstrativo3" ] = "";
+		
+		// Instruções de pagamento.
+		$dadosboleto[ "instrucoes1" ] = "- Sr. Caixa, cobrar multa de 2% após o vencimento";
+		$dadosboleto[ "instrucoes2" ] = "- Receber até 10 dias após o vencimento";
+		$dadosboleto[ "instrucoes3" ] = "- Em caso de dúvidas entre em contato conosco: suporte@hcode.com.br";
+		$dadosboleto[ "instrucoes4" ] = "&nbsp; Emitido pelo sistema Projeto Loja Hcode E-commerce - www.hcode.com.br";
 
 		// DADOS OPCIONAIS DE ACORDO COM O BANCO OU CLIENTE
-		$dadosboleto["quantidade"] = "";
-		$dadosboleto["valor_unitario"] = "";
-		$dadosboleto["aceite"] = "";		
-		$dadosboleto["especie"] = "R$";
-		$dadosboleto["especie_doc"] = "";
 
+		$dadosboleto[ "quantidade"]      = "";
+		$dadosboleto[ "valor_unitario" ] = "";
+		$dadosboleto[ "aceite" ]         = "";		
+		$dadosboleto[ "especie" ]        = "R$";
+		$dadosboleto[ "especie_doc" ]    = "";
 
 		// ---------------------- DADOS FIXOS DE CONFIGURAÇÃO DO SEU BOLETO --------------- //
 
-
 		// DADOS DA SUA CONTA - ITAÚ
-		$dadosboleto["agencia"] = "1690"; // Num da agencia, sem digito
-		$dadosboleto["conta"] = "48781";	// Num da conta, sem digito
-		$dadosboleto["conta_dv"] = "2"; 	// Digito do Num da conta
+		
+		// Numero da agencia, sem digito.
+		$dadosboleto[ "agencia" ]  = "1690"; 
+
+		// Numero da conta, sem digito
+		$dadosboleto[ "conta" ] = "48781";	
+		
+		// Digito do Numero da conta
+		$dadosboleto[ "conta_dv" ] = "2"; 	
 
 		// DADOS PERSONALIZADOS - ITAÚ
-		$dadosboleto["carteira"] = "175";  // Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+		
+		// Código da Carteira: pode ser 175, 174, 104, 109, 178, ou 157
+		$dadosboleto[ "carteira" ] = "175";  
 
 		// SEUS DADOS
-		$dadosboleto["identificacao"] = "Hcode Treinamentos";
-		$dadosboleto["cpf_cnpj"] = "24.700.731/0001-08";
-		$dadosboleto["endereco"] = "Rua Ademar Saraiva Leão, 234 - Alvarenga, 09853-120";
-		$dadosboleto["cidade_uf"] = "São Bernardo do Campo - SP";
-		$dadosboleto["cedente"] = "HCODE TREINAMENTOS LTDA - ME";
+		$dadosboleto[ "identificacao"] = "Hcode Treinamentos";
+		$dadosboleto[ "cpf_cnpj" ]     = "24.700.731/0001-08";
+		$dadosboleto[ "endereco" ]     = "Rua Ademar Saraiva Leão, 234 - Alvarenga, 09853-120";
+		$dadosboleto[ "cidade_uf" ]    = "São Bernardo do Campo - SP";
+		$dadosboleto[ "cedente" ]      = "HCODE TREINAMENTOS LTDA - ME";
 
 		// NÃO ALTERAR!
 		$path = $_SERVER[ 'DOCUMENT_ROOT' ] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR;
 		
 		require_once( $path . "funcoes_itau.php" ); 
 		require_once( $path . "layout_itau.php" );
+
+	} );
+
+	$app->get( "/profile/orders", function() {
+
+		User::verifyLogin( false );
+
+		$user = User::getFromSession();
+
+		$page = new Page();
+		$page->setTpl( "profile-orders", [
+			'orders' => $user->getOrders()
+		] );
+
+	} );
+
+	$app->get( "/profile/orders/:idorder", function( $idorder ) {
+
+		User::verifyLogin( false );
+
+		$order = new Order();
+		$order->get( (int)$idorder );
+
+		$cart = new Cart();
+		$cart->get( (int)$order->getidcart() );
+		$cart->getCalculateTotal();
+
+
+
+		$page = new Page();
+		$page->setTpl( "profile-orders-detail", [
+			'order'    => $order->getValues(),
+			'cart'     => $cart->getValues(),
+			'products' => $cart->getProducts()
+		] );
 
 	} );
 
