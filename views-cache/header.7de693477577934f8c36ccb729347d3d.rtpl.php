14c9796f46734d65506903526f2a8aabe95e2cbb
fdavidcunha <f.david.cunha@gmail.com>
2017-12-04 05:40:22 -0300

    Mudanças no leiaute

--- views-cache/header.7de693477577934f8c36ccb729347d3d.rtpl.php
+++ views-cache/header.7de693477577934f8c36ccb729347d3d.rtpl.php
@@ -7,24 +7,36 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hcode Store</title>
-    
+    <!-- Description, Keywords and Author -->
+    <meta name="description" content="Para quem quer ter uma alimentação saudável e rápida e solcução ideal são os nossos lanches e refeições.">
+    <meta name="keywords" content="sabor, saúde, coxinha, sucos, lanche, saudável, sem óleo, sem fritura, recife, antigo">
+    <meta name="author" content="David Ferreira f.david.cunha@gmail.com">
+
+    <title>Sabor & Saúde - Comida saudável</title>
+
     <!-- Google Fonts -->
     <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,700,600' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,100' rel='stylesheet' type='text/css'>
     
+    <!-- Styles -->
     <!-- Bootstrap -->
     <link rel="stylesheet" href="/res/site/css/bootstrap.min.css">
-    
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/res/site/css/font-awesome.min.css">
-    
     <!-- Custom CSS -->
     <link rel="stylesheet" href="/res/site/css/owl.carousel.css">
     <link rel="stylesheet" href="/res/site/css/style.css">
     <link rel="stylesheet" href="/res/site/css/responsive.css">
-
+    <!-- SLIDER REVOLUTION 4.x CSS SETTINGS -->
+    <link rel="stylesheet" href="/res/site/css/settings.css">     
+    <!-- FlexSlider Css -->
+    <link rel="stylesheet" href="/res/site/css/flexslider.css" media="screen" />
+    <!-- Portfolio CSS -->
+    <link rel="stylesheet" href="/res/site/css/prettyPhoto.css">
+    <!-- Custom Less -->
+    <link rel="stylesheet" href="/res/site/css/less-style.css">   
+    <!--[if IE]><link rel="stylesheet" href="css/ie-style.css"><![endif]-->
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -40,12 +52,14 @@
   </head>
   <body>
    
+    <!-- End header area -->
     <div class="header-area">
         <div class="container">
             <div class="row">
                 <div class="col-md-8">
                     <div class="user-menu">
                         <ul>
+                            
                             <li>
                                 <a href="/profile">
                                     <i class="fa fa-user"></i> Minha Conta
@@ -85,59 +99,55 @@
                 
                 <div class="col-md-4">
                     <div class="header-right">
+                        
                         <ul class="list-unstyled list-inline">
+                            
                             <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">moeda :</span><span class="value">BRL </span><b class="caret"></b></a>
+                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#">
+                                    <span class="key">
+                                        <i class="fa fa-money" aria-hidden="true"></i>
+                                    </span>
+                                    <span class="value">Real - BR</span>
+                                    <b class="caret"></b>
+                                </a>
                                 <ul class="dropdown-menu">
-                                    <li><a href="#">BRL</a></li>
-                                    <li><a href="#">USD</a></li>
+                                    <li>
+                                        <a href="#">Real - BR</a>
+                                    </li>
+                                    <!--<li><a href="#">USD</a></li>-->
                                 </ul>
                             </li>
 
                             <li class="dropdown dropdown-small">
-                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#"><span class="key">linguagem :</span><span class="value">Português </span><b class="caret"></b></a>
+                                <a data-toggle="dropdown" data-hover="dropdown" class="dropdown-toggle" href="#">
+                                    <span class="key">
+                                        <i class="fa fa-language" aria-hidden="true"></i>
+                                    </span>
+                                    <span class="value">Português</span>
+                                    <b class="caret"></b>
+                                </a>
                                 <ul class="dropdown-menu">
                                     <li><a href="#">Português</a></li>
-                                    <li><a href="#">Inglês</a></li>
-                                    <li><a href="#">Espanhol</a></li>
+                                    <!--<li><a href="#">Inglês</a></li>
+                                    <li><a href="#">Espanhol</a></li>-->
                                 </ul>
                             </li>
+
                         </ul>
                     </div>
                 </div>
+
             </div>
         </div>
-    </div> <!-- End header area -->
+    </div> 
+    <!-- End header area -->
     
-    <div class="site-branding-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="logo">
-                        <h1>
-                            <a href="/">
-                                <img src="/res/site/img/logo.png">
-                            </a>
-                        </h1>
-                    </div>
-                </div>
-                
-                <div class="col-sm-6">
-                    <div class="shopping-item">
-                        <a href="/cart">Carrinho - 
-                            <span class="cart-amunt">R$ <?php echo getCartVlSubTotal(); ?></span> 
-                            <i class="fa fa-shopping-cart"></i> 
-                            <span class="product-count"><?php echo getCartNrQtd(); ?></span>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> <!-- End site branding area -->
-    
+   
     <div class="mainmenu-area">
         <div class="container">
+            
             <div class="row">
+                
                 <div class="navbar-header">
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                         <span class="sr-only">Toggle navigation</span>
@@ -146,19 +156,45 @@
                         <span class="icon-bar"></span>
                     </button>
                 </div> 
-                <div class="navbar-collapse collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active">
-                            <a href="/">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">Produtos</a>
-                        </li>
-                        <li>
-                            <a href="/cart">Carrinho</a>
-                        </li>
-                    </ul>
-                </div>  
+
+                <div class="col-md-8">
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="active">
+                                <a href="/">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Cardápio</a>
+                            </li>
+                            <li>
+                                <a href="#">Promoções</a>
+                            </li>
+                            <li>
+                                <a href="#">Sobre nós</a>
+                            </li>
+                            <li>
+                                <a href="/cart">Carrinho</a>
+                            </li>
+                        </ul>
+                    </div>  
+                </div>
+
+                <div class="col-md-4">
+                    <div class="navbar-collapse collapse">
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a href="/cart">
+                                    <i class="fa fa-shopping-cart"></i> 
+                                    <span>Carrinho - </span>
+                                    <span class="cart-amunt">R$ <?php echo getCartVlSubTotal(); ?></span> 
+                                    <span class="product-count"><?php echo getCartNrQtd(); ?></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
             </div>
+
         </div>
     </div> <!-- End mainmenu area -->
